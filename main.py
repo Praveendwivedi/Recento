1c10554561a7242647106dfdf05dda50bfc4d787
Praveendwivedi <49608665+Praveendwivedi@users.noreply.github.com>
2021-07-01 16:40:10 +0530

    Update main.py
    added twitter api

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import discord
 import os
+import requests,json
+import tweepy
 
+
+consumer_key=os.getenv('C_K')
+consumer_secret=os.getenv('C_S')
+access_token=os.getenv('A_T')
+access_token_secret=os.getenv('A_S')
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+user = api.get_user('Praveen')
+print(user.screen_name)
+print(user.followers_count)
+
+# public_tweets = api.home_timeline()
+# print('5')
+# for tweet in public_tweets:
+#     print(tweet.text).encode(encoding='UTF-8',errors='ignore')
+r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+print(r.json())
+print(r.status_code)
 client = discord.Client()
 
 @client.event
@@ -12,8 +34,14 @@ async def on_message(msg):
   if msg.author==client.user:
     return
   if msg:
-    await msg.channel.send('hey')
+    print(msg)
+    await msg.channel.send('hey {}'.format(msg.author.name))
+  if 'inspire' in msg.content.lower():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    await msg.channel.send(quote)
+  
   
   
 client.run(os.getenv('TOKEN'))
-
